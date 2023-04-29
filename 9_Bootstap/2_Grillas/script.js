e9cde4264a4d4ae8359706e47c6465c90eb4c2ee
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-04-29 17:14:55 -0300

    prettier

--- 9_Bootstap/2_Grillas/script.js
+++ 9_Bootstap/2_Grillas/script.js
@@ -1,23 +1,23 @@
 // Si, esto es completamente inecesario, pero tenia ganas de hacerlo
-const px = document.getElementById('pixeles');
-const breakpoint = document.getElementById('breakpoint');
+const px = document.getElementById('pixeles')
+const breakpoint = document.getElementById('breakpoint')
 
 const update = () => {
-	const screenWidth = window.innerWidth;
-	px.innerText = screenWidth;
-	if (screenWidth < 576) {
-		breakpoint.innerText = 'Extra Small';
-	} else if (screenWidth < 768) {
-		breakpoint.innerText = 'Small';
-	} else if (screenWidth < 992) {
-		breakpoint.innerText = 'Medium';
-	} else if (screenWidth < 1200) {
-		breakpoint.innerText = 'Large';
-	} else if (screenWidth < 1400) {
-		breakpoint.innerText = 'Extra Large';
-	} else {
-		breakpoint.innerText = 'Extra Extra Large';
-	}
-};
-update();
-window.addEventListener('resize', update);
+  const screenWidth = window.innerWidth
+  px.innerText = screenWidth
+  if (screenWidth < 576) {
+    breakpoint.innerText = 'Extra Small'
+  } else if (screenWidth < 768) {
+    breakpoint.innerText = 'Small'
+  } else if (screenWidth < 992) {
+    breakpoint.innerText = 'Medium'
+  } else if (screenWidth < 1200) {
+    breakpoint.innerText = 'Large'
+  } else if (screenWidth < 1400) {
+    breakpoint.innerText = 'Extra Large'
+  } else {
+    breakpoint.innerText = 'Extra Extra Large'
+  }
+}
+update()
+window.addEventListener('resize', update)
